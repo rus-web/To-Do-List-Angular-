690a6a43ec1222f4319bc865ae3240927dba8611
Ruslan Tinaev <rus.tinaev.web@gmail.com>
2025-04-27 20:14:34 +0300

    + Добавил 2 массива в компонент, сократил HTML, внес правки в CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
   standalone: false,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
 })
+
 export class AppComponent {
   title = 'To-Do-List';
+  tasksUndone = ["Домашние дела","Разобраться с циклами JS","Покушать","Изучить что такое DOM"]
+  tasksDone = ["Повторение CSS и HTML","Повторение HTML"]
 }
