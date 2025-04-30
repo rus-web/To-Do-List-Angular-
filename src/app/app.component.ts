bc218232b4ed8869b8cdb070d0195c4e6e8f019d
Ruslan Tinaev <rus.tinaev.web@gmail.com>
2025-04-30 20:23:48 +0300

    + добавил логику

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,18 @@ export class AppComponent {
   title = 'To-Do-List';
   tasksUndone = ["Домашние дела","Разобраться с циклами JS","Покушать","Изучить что такое DOM"]
   tasksDone = ["Повторение CSS и HTML","Повторение HTML"]
+  
+  statusChange(taskStatus: string, task: string){
+    
+      if(taskStatus==="undone"){
+        this.tasksUndone.splice(this.tasksUndone.indexOf(task),1)
+        this.tasksDone.push(task)
+      } else{
+      this.tasksDone.splice(this.tasksDone.indexOf(task),1)
+      this.tasksUndone.push(task)
+      }
+    
+
+  }
 }
+
