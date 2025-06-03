dbd4242376f4093d77b2b662e32217dbee903332
Ruslan <rus.tinaev.web@gmail.com>
2025-06-03 11:55:56 +0300

    + добавил метод ngOnDestroy в компонент

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WebsocketService } from './services/websocket.service';
 
 
@@ -9,15 +9,25 @@ import { WebsocketService } from './services/websocket.service';
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
 
   constructor(private wsService: WebsocketService) { }
+
+
+  ngOnInit(): void {
+     console.log('Инициализация прошла успешно')
+  };
+
+  ngOnDestroy(): void {
+  this.wsService.closeConnection()
+  };
+
 
   title = 'To-Do-List';
   tasksUndone = ["Домашние дела", "Разобраться с циклами JS", "Покушать", "Изучить что такое DOM"];
   tasksDone = ["Повторение CSS и HTML", "Повторение HTML"];
 
-  statusChange(taskStatus: string, task: string):void {
+  statusChange(taskStatus: string, task: string): void {
     let index;
 
     if (taskStatus === "undone") {
@@ -28,21 +38,21 @@ export class AppComponent {
       let obj = {
         status: "done",
         text: task
-      }
+      };
 
       this.wsService.sendWS(obj)
     } else {
       index = this.tasksDone.indexOf(task);
       this.tasksDone.splice(index, 1);
       this.tasksUndone.push(task);
-      
+
       let obj = {
         status: "undone",
         text: task
-      }
-      
+      };
+
       this.wsService.sendWS(obj)
-    }
+    };
 
   }
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable,Subject } from 'rxjs';
-import { AppComponent } from '../app.component';
 
 @Injectable({
   providedIn: 'root'
 })
-
 
 export class WebsocketService {
 
@@ -25,8 +22,14 @@ export class WebsocketService {
     console.log("Server sent:", message.data);
   };
 }
+
   sendWS(task: object): 
   void{
     this.socket.send(JSON.stringify(task))
-  }
+  };
+
+  closeConnection(): 
+  void{
+    this.socket.close;
+  };
 }
